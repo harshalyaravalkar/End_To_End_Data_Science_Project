c78891622447f1bda03550e1c1690ae8143f2671
Harshal Yaravalkar <143155629+harshalyaravalkar@users.noreply.github.com>
2026-07-09 21:14:05 +0530

    Refactor train_model.py for clarity and consistency

--- train_model.py
+++ train_model.py
@@ -6,28 +6,21 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-# Load Data
+# Loading Dataset
 
 df = pd.read_csv("rentdata.csv")
 
-# Data Cleaning
+# Cleaning Dataset
 
 df = df[df["BHK"] != 6]
 
 # remove unused columns
 df.drop(columns=["Posted On"], inplace=True)
 
-# keep only useful columns
-df = df[[
-    "BHK",
-    "Size",
-    "City",
-    "Furnishing Status",
-    "Bathroom",
-    "Rent"
-]]
+# keeping only useful columns
+df = df[["BHK", "Size", "City", "Furnishing Status", "Bathroom", "Rent"]]
 
-# Data Encoding
+# Encoding categorical columns
 
 city_encoder = LabelEncoder()
 furnish_encoder = LabelEncoder()
@@ -35,19 +28,14 @@ furnish_encoder = LabelEncoder()
 df["City"] = city_encoder.fit_transform(df["City"])
 df["Furnishing Status"] = furnish_encoder.fit_transform(df["Furnishing Status"])
 
-# Split data in training and testing
+# splitting into train and test
 
 X = df.drop("Rent", axis=1)
 y = df["Rent"]
 
-xtrain, xtest, ytrain, ytest = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
+xtrain, xtest, ytrain, ytest = train_test_split(X,y,test_size=0.2,random_state=42)
 
-# Model
+# model training
 
 model = RandomForestRegressor(
     n_estimators=100,
@@ -56,7 +44,7 @@ model = RandomForestRegressor(
 
 model.fit(xtrain, ytrain)
 
-# Model Evaluation
+# model evaluation
 
 pred = model.predict(xtest)
 
@@ -64,7 +52,7 @@ error = mean_absolute_error(ytest, pred)
 
 print("Mean Absolute Error:", error)
 
-# Save Model
+# Saving model pkl files
 
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
